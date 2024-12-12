3d4625b412c27200daae17a5867e41b4a5710267
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-12-12 14:01:10 +0100

    changed Test

--- shop_test.py
+++ shop_test.py
@@ -16,22 +16,22 @@ def test_list(monkeypatch, capsys):
     inputs = iter(['Ei', 0.75, 30, 'Käse', '45.95', '7', 'Ei', '-4', 'Käse', -1, 'Exit'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     list = shop.main().__repr__()
-    assert list == '[Article(name=\'Ei\', price=0.75, stock=26.0), Article(name=\'Käse\', price=45.95, stock=6.0)]'
+    assert list == '[Article(name=\'Ei\', price=0.75, stock=26), Article(name=\'Käse\', price=45.95, stock=6)]'
 
 def test_output(monkeypatch, capsys):
     inputs = iter(['Ei', '0.75', '30', 'Käse', '45.95', '7', 'Ei', '-4', 'Käse', '-1', 'Exit'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     shop.main()
     captured = capsys.readouterr()
-    assert captured.out == 'Bestand     : 30.0\nBestand     : 7.0\n'
+    assert captured.out == 'Bestand     : 30\nBestand     : 7\n'
 
 def test_output_inventory(monkeypatch, capsys):
     inputs = iter(['Ei', '0.75', '30', 'Käse', '45.95', '7', 'Ei', '-4', 'Käse', '-1', 'Inventory', 'Exit'])
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     shop.main()
     captured = capsys.readouterr()
-    assert captured.out == ('Bestand     : 30.0\n'
-                            'Bestand     : 7.0\n'
+    assert captured.out == ('Bestand     : 30\n'
+                            'Bestand     : 7\n'
                             'Ei : 19.5\n'
                             'Käse : 275.70000000000005\n'
                             'Gesamt : 295.20000000000005\n'
